61f6f62880734469ccdcd0cea92f36a638758446
jsakata <jsakata@umich.edu>
2014-10-22 14:25:54 -0400

    Created Popup Window for Check-in in study detail

--- Cahoot-App/app/src/main/java/com/example/lukerehmann/cahoot/studydetail.java
+++ Cahoot-App/app/src/main/java/com/example/lukerehmann/cahoot/studydetail.java
@@ -1,16 +1,14 @@
 package com.example.lukerehmann.cahoot;
 
 import android.app.Activity;
-import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 // Luke Rehmann
 
@@ -43,6 +41,16 @@ public void checkin(){
 
 
 public class studydetail extends Activity {
+
+    public void alertButton(View view){
+        new AlertDialog.Builder(this)
+        .setTitle("Success!")
+        .setMessage("You Successfully Checked In!")
+        .setNeutralButton("OK", null)
+        .show();
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
