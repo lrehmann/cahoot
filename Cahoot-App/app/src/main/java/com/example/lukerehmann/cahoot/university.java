aa1ade5b9f04a51897ee53eca0890a2653f67942
jsakata <jsakata@umich.edu>
2014-11-18 13:19:07 -0500

    Fixed android hint text for text input boxes

--- Cahoot-App/app/src/main/java/com/example/lukerehmann/cahoot/university.java
+++ Cahoot-App/app/src/main/java/com/example/lukerehmann/cahoot/university.java
@@ -1,29 +1,17 @@
 package com.example.lukerehmann.cahoot;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
-<<<<<<< Updated upstream
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-
-=======
-import java.util.ArrayList;
-import java.util.List;
-import android.os.Bundle;
-import android.view.View;
-import android.content.SharedPreferences;
-import android.widget.Button;
 import android.widget.EditText;
->>>>>>> Stashed changes
+
 
 // Mingjue Ni
 
