6ade151501f535fdf527f38dc4646d87882ce452
Mingjuen <MJ@MJs-MacBook-Pro.local>
2014-11-23 18:56:14 -0500

    logout dialog_WIP

--- Cahoot-App/app/src/main/java/com/example/lukerehmann/cahoot/DialogBox.java
+++ Cahoot-App/app/src/main/java/com/example/lukerehmann/cahoot/DialogBox.java
@@ -13,3 +13,42 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.lukerehmann.cahoot.R;
+
+public void logOutDialog() {
+
+    AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+
+    //Setting dialog title
+    alertdialog.setTitle("Confirm Logout...")
+
+    //Setting Dialog Message
+    alertDialog.setMessage("Are you sure you want to log out");
+
+    //setting positive "Yes" Button
+    alertDialog.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener(){
+        public void onClick(DialogInterface dialog, int which) {
+            //write code to invoke logout event and go back to the login activity
+            logOut();
+        }
+    });
+
+    //setting negative "Cancel" Button
+    alertdialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int which) {
+            dialog.cancel();
+        }
+    });
+
+    //showing alert message
+    alertDialog.show();
+
+}
+
+public void logOut() {
+
+    Toast.makeText(getApplicationContext(), "You've logged out!", Toast.LENGTH_SHORT).show();
+
+    Intent intent = new Intent(this, login.class);
+    startActivity(intent);
+
+    finish();
